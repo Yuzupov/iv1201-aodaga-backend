369555473937cd9bd001bf260e17e732af9e938f
Alexander Rocabado Hellgren <alrh@kth.se>
2025-02-13 13:00:07 +0100

    separated the backend to its own repo, requires copying over manually from main because author forgot before making new repo

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === "development") {
-    const { worker } = require("../mocks/browser");
-    worker.start();
-    console.log("Mock Service Worker is running in development mode.");
-  }
-  
-  
-  
